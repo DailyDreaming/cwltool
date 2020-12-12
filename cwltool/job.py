eeba11f51715cec2f5c985476b59d53ff1f14195
DailyDreaming <lblauvel@ucsc.edu>
2020-12-12 00:49:47 -0800

    Null context import.

--- cwltool/job.py
+++ cwltool/job.py
@@ -61,10 +61,8 @@ try:
     from contextlib import nullcontext
 except ImportError:  # only in python 3.7+
     class nullcontext:
-        def __init__(self, enter_result=None):
-            self.enter_result = enter_result
         def __enter__(self):
-            return self.enter_result
+            return
         def __exit__(self, *excinfo):
             pass
 
